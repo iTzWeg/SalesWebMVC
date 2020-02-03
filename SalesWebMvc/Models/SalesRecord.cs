49f9c12787838deb7fc4d6f6323b6d242b84b963
Wellington Andrade <Weg.andrade@gmail.com>
2020-02-03 19:25:18 -0300

    Other Entities e Migrations

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,27 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SalesStatus Status { get; set; }
+        public Seller Seller { get; set; }
+        public SalesRecord()
+        {
+
+        }
+
+        public SalesRecord(int iD, DateTime date, double amount, SalesStatus status, Seller seller)
+        {
+            ID = iD;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
